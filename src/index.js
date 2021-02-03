c9981b8a0e0c7948dcf20cce46f2732fe69d1523
Marina Grishina <52827985+AniramOcean@users.noreply.github.com>
2021-02-03 22:00:39 +0300

    fix: add semicolon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function towelSort (matrix) {
-    let arr = []
+    let arr = [];
     if (matrix && matrix.length > 0) {
         matrix.forEach((item, index) => index % 2 !== 0 ? arr.push(item.reverse()) : arr.push(item))
     }
